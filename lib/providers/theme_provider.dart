a12ef9c42339186c588869c67f71bb4081a5b671
OnoPUNPUN <punpunonodera754@gmail.com>
2025-11-25 00:11:04 +0600

    Created Theme Provider

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,1 +1,46 @@
-// TODO: Implement ThemeProvider
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, ThemeMode>(
+  (ref) => ThemeNotifier(),
+);
+
+class ThemeNotifier extends StateNotifier<ThemeMode> {
+  static const _kThemeKey = 'theme_mode';
+  ThemeNotifier() : super(ThemeMode.system) {
+    _load();
+  }
+
+  Future<void> _load() async {
+    final prefs = await SharedPreferences.getInstance();
+    final s = prefs.getString(_kThemeKey);
+    if (s == 'light') {
+      state = ThemeMode.light;
+    } else if (s == 'dark') {
+      state = ThemeMode.dark;
+    } else {
+      state = ThemeMode.light;
+    }
+  }
+
+  Future<void> toggle() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (state == ThemeMode.light) {
+      state = ThemeMode.dark;
+      await prefs.setString(_kThemeKey, 'dark');
+    } else {
+      state = ThemeMode.light;
+      await prefs.setString(_kThemeKey, 'light');
+    }
+  }
+
+  Future<void> setMode(ThemeMode mode) async {
+    final prefs = await SharedPreferences.getInstance();
+    state = mode;
+    await prefs.setString(
+      _kThemeKey,
+      mode == ThemeMode.light ? 'light' : 'dark',
+    );
+  }
+}
