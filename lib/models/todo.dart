460be52e60bee0bf71a59adcb348eb4b350d3828
OnoPUNPUN <punpunonodera754@gmail.com>
2025-11-24 23:45:28 +0600

    Created The Model For the Todo

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,1 +1,38 @@
-// TODO: Implement Todo Model
+class Todo {
+  final int id;
+  final String todo;
+  final bool completed;
+  final int userId;
+
+  const Todo({
+    required this.id,
+    required this.todo,
+    required this.completed,
+    required this.userId,
+  });
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(
+      id: json['id'] as int,
+      todo: json['todo'] as String,
+      completed: json['completed'] as bool,
+      userId: (json['userId'] ?? 0) as int,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'todo': todo,
+    'completed': completed,
+    'userId': userId,
+  };
+
+  Todo copyWith({int? id, String? todo, bool? completed, int? userId}) {
+    return Todo(
+      id: id ?? this.id,
+      todo: todo ?? this.todo,
+      completed: completed ?? this.completed,
+      userId: userId ?? this.userId,
+    );
+  }
+}
