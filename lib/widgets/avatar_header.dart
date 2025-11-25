a5279df262e6bce4f0a3e152f0bf03cf98edf2dc
OnoPUNPUN <punpunonodera754@gmail.com>
2025-11-25 16:02:24 +0600

    Created The loading widget

--- lib/widgets/avatar_header.dart
+++ lib/widgets/avatar_header.dart
@@ -1,1 +1,1 @@
-// TODO: Implement AvatarHeader
+
--- lib/widgets/loading.dart
+++ lib/widgets/loading.dart
@@ -1,1 +1,9 @@
-// TODO: Implement LoadingWidget
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  const Loading({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: CircularProgressIndicator());
+  }
+}
